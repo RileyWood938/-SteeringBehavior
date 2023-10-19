37e14a8ffafa5d5900e258e6788e6e6afb5ecf59
RileyWood938 <71392554+RileyWood938@users.noreply.github.com>
2023-10-19 11:03:53 +0100

    the important bits

---
+++ Vehicle.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour {
+
+    /////////////////////
+    //Updated Values
+    /////////////////////
+    /// <summary>
+    /// This is applied to the current position every frame
+    /// </summary>
+    public Vector3 Velocity;
+
+    //Position, Heading and Side can be accessed from the transform component with transform.position, transform.forward and transform.right respectively
+
+    //"Constant" values, they are public so we can adjust them through the editor
+
+    //Represents the weight of an object, will effect its acceleration
+    public float Mass = 1;
+
+    //The maximum speed this agent can move per second
+    public float MaxSpeed = 1;
+
+    //The thrust this agent can produce
+    public float MaxForce = 1;
+
+    //We use this to determine how fast the agent can turn, but just ignore it for, we won't be using it
+    public float MaxTurnRate = 1.0f;
+
+    private SteeringBehaviourBase[] SteeringBehaviors;
+    private void Start()
+    {
+        //.Get components returns a reliably ordered top-to-bottom list of all the SteeringBehaviors in the editor
+        SteeringBehaviors = GetComponents<SteeringBehaviourBase>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Vector3 SteeringForce = Vector3.zero;
+
+        //Get all steering behaviours attached to this object and add their calculated steering force onto this SteeringForce
+
+        foreach (SteeringBehaviourBase i in SteeringBehaviors)
+        {
+            Vector3 inputForce = i.Calculate() * i.getWeight();
+
+            if((SteeringForce + inputForce).magnitude > MaxForce)
+            {
+                Vector3.ClampMagnitude(inputForce, (MaxForce - inputForce.magnitude));
+                SteeringForce += inputForce;
+                break;
+            }
+            else
+            {
+                SteeringForce += inputForce;
+            }
+        }
+
+        Vector3 Acceleration = SteeringForce / Mass;
+
+        Velocity += Acceleration * Time.deltaTime;
+
+        Velocity = Vector3.ClampMagnitude(Velocity, MaxSpeed);
+
+        if (Velocity != Vector3.zero)
+        {
+            transform.position += Velocity * Time.deltaTime;
+
+            transform.forward = Velocity.normalized;
+        }
+
+        //transform.right should update on its own once we update the transform.forward
+	}
+}
---
+++ Wander.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wander : SteeringBehaviourBase
+{
+    public float WanderRadius = 10f;
+    public float WanderDistance = 10f;
+    public float WanderJitter = 1f;
+    Vector3 WanderTarget = Vector3.zero;
+    float WanderAngle = 0.0f;
+    [SerializeField]
+    private float maxTurnRadius;
+
+    void Start()
+    {
+        WanderAngle = Random.Range(0.0f, Mathf.PI * 2);
+        WanderTarget = new Vector3(Mathf.Cos(WanderAngle), 0, Mathf.Sin(WanderAngle)) * WanderRadius;
+    }
+
+    public override Vector3 Calculate()
+    {
+        Vector3 TrueAngle = 
+        WanderTarget = new Vector3(Mathf.Cos(WanderAngle), 0, Mathf.Sin(WanderAngle)) * WanderRadius;
+
+        Vector3 targetWorld = transform.position + WanderTarget;
+
+        targetWorld += transform.forward * WanderDistance;
+
+        return targetWorld - transform.position;
+    }
+
+    IEnumerator updateWanderPosition()
+    {
+        yield return new WaitForSeconds(1.0f);
+        WanderAngle += Random.Range(-WanderJitter, WanderJitter);
+    }
+}
